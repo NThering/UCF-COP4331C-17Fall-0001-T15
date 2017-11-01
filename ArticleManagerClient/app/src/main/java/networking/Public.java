70d3af99fecf26099dbba7174afc67f93873d629
Spoupovitch <trevormjenkins@yahoo.com>
2017-11-01 19:36:17 -0400

    first pass for connection establishment and session management

--- ArticleManagerClient/app/src/main/java/networking/Public.java
+++ ArticleManagerClient/app/src/main/java/networking/Public.java
@@ -1,42 +1,76 @@
 package networking;
+
 import java.io.File;
 import java.util.ArrayList;
 import am_utils.ArticleInfo;
 import am_utils.CategoryEnums.MainCategory;
 import am_utils.CategoryEnums.SubCategory;
+import java.sql.*;
+import com.mysql.jdbc.Driver;
 
 public class Public {
     /*    General Responsibilities:
-    *     Keep track of logged in users and their permissions so they don't need to re-authenticate for every action.
+    *     Keep track of logged in users and their permissions so they don't need to re-authenticate
+    *     for every action.
     */
 
     // All calls to networking should probably be done in a seperate thread to prevent hangups!
 
-    /** Relays user info to the login system on the server, returning 0 on the client if the login was successful and an int corresponding to the type of error if it was not. */
+    private String driver = "com.mysql.jdbc.Driver";
+    private String URL = "jdbc:mysql://localhost:8080/ArticleManager";
+    private String USER = "root";
+    private String PASS = "";
+
+    public static Connection conn = null;
+
+    private SessionManager session = new SessionManager(context);
+
+    try {
+        //load jdbc driver
+        Class.forName(driver);
+
+        //establish connection to database
+        conn = DriverManager.getConnection(URL, USER, PASS);
+
+        serverSocket = new ServerSocket(8080);
+    }
+    catch (SQLException e) {
+        System.out.println("Error occured: ");
+        System.out.println(e.printStackTrace());
+    }
+
+    /** Relays user info to the login system on the server, returning 0 on the client if the login
+     * was successful and an int corresponding to the type of error if it was not. */
     public static int login( String username, String password )
     {
         return 1;
     }
 
-    /** Attempts to log out of the server, returning 0 on the client if the logout was successful and an int corresponding to the type of error if it was not. */
+    /** Attempts to log out of the server, returning 0 on the client if the logout was successful
+     * and an int corresponding to the type of error if it was not. */
     public static int logout( )
     {
         return 1;
     }
 
-    /** Relays user info to the login system on the server, returning 0 on the client if the registration was successful and an int corresponding to the type of error if it was not. */
+    /** Relays user info to the login system on the server, returning 0 on the client if the
+     * registration was successful and an int corresponding to the type of error if it was not. */
     public static int register( String username, String password )
     {
         return 1;
     }
 
-    /** Returns a list to the client of all ArticleInfos in a given category/sub-category.  If IncludeAbstracts is false, abstracts are left blank to save bandwidth.*/
-    public static ArrayList<ArticleInfo> getArticlesFromCategory( MainCategory articleCategory, SubCategory subCategory, boolean IncludeAbstracts )
+    /** Returns a list to the client of all ArticleInfos in a given category/sub-category.  If
+     * IncludeAbstracts is false, abstracts are left blank to save bandwidth.*/
+    public static ArrayList<ArticleInfo> getArticlesFromCategory( MainCategory articleCategory,
+          SubCategory subCategory, boolean IncludeAbstracts )
     {
         return null;
     }
 
-    /** Sends the article to the database for entry, returns 0 if successful and an int corresponding to the type of error if it was not.  If ArticleID in the supplied ArticleInfo is not 0 this upload is meant to replace an existing upload. */
+    /** Sends the article to the database for entry, returns 0 if successful and an int
+     * corresponding to the type of error if it was not.  If ArticleID in the supplied ArticleInfo
+     * is not 0 this upload is meant to replace an existing upload. */
     public static int uploadArticle( File articleFile, ArticleInfo articleInfo )
     {
         return 1;
@@ -60,8 +94,8 @@ public class Public {
         return 0;
     }
 
-    /** Abort any ongoing networking activity  Do not terminate the user's session, just cancel whatever transfers were taking place
-     * in case the user hits the back button or something. */
+    /** Abort any ongoing networking activity  Do not terminate the user's session, just cancel
+     * whatever transfers were taking place in case the user hits the back button or something. */
     public static void abortActiveConnections()
     {
         return;
