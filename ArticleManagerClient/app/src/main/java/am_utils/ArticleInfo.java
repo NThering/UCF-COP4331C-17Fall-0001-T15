504dbf0c53716dc4822af8fa66b6e26d7c5930c5
nthering <nthering@knights.ucf.edu>
2017-11-03 15:33:23 -0400

    Updated ArticleInfo so the purpose of each field is more clear.

--- ArticleManagerClient/app/src/main/java/am_utils/ArticleInfo.java
+++ ArticleManagerClient/app/src/main/java/am_utils/ArticleInfo.java
@@ -8,32 +8,53 @@ import java.util.Date;
  *
  * @author NThering
  */
+
 public class ArticleInfo {
-    /** The unique internal ID of the article.  No two articles must ever share the same ID, as this way they can share the same print name and still be uniquely addressable. */
+    /** The unique internal ID of the article.  No two articles must ever share the same ID, as this way they can share the same print name and still be uniquely addressable.
+     * Used in identifying the article for all critical article functions.
+     */
     private int articleID;
 
-    /** DOI number of the article, if any. */
+    /** DOI number of the article, if any.
+     * Assigned a value of null if no DOI number exists.
+     * Used for future proofing the application
+     */
     public String doiNumber;
 
-    /**The name that the article is displayed under, can be the same as other articles.*/
+    /**The name that the article is displayed under, can be the same as other articles.
+     * Used for display in the article lists, along with display in the article info and text files.
+     */
     public String printName;
 
-    /** Category the article is assigned to */
+    /** Category the article is assigned to.
+     * Critical for category display and storage.
+     */
     public MainCategory mainCategory;
 
-    /** Sub-Category the article is assigned to */
+    /** Sub-Category the article is assigned to
+     * Critical for category display and storage.
+     */
     public SubCategory subCategory;
 
-    /** Authors of the article */
+    /** Authors of the article
+     * Used primarily to display the more specific details of an article when it's selected, but
+     * is also used to make that one text file the assignment specs want us to have.
+     */
     public String author;
 
-    /** Account who uploaded the article */
+    /** Account who uploaded the article
+     * Used for permissions tracking in the database and display of the article's information.
+     */
     public String owner;
 
-    /** Text of the abstract section of the document, limited to 5000 characters.  "..." will replace the last 3 characters if the 5000 limit is exceeded.*/
+    /** Text of the abstract section of the document, limited to 5000 characters.  "..." will replace the last 3 characters if the 5000 limit is exceeded.
+     * Used for the display of an article's properties along with making the -other- pointless text file required by the assignment.
+     */
     public String abstractText;
 
-    /** Time the article was uploaded or last updated. */
+    /** Time the article was uploaded or last updated.
+     * Used for record keeping/article info display.
+     */
     public Date uploadTime;
 
     /** articleID should be 0 on fresh upload, once a value other than 0 is assigned it cannot be changed.
