77f7246525421d9cc95f6b817a08ff1136eb4d1c
Kelsey Cameron <kelcamer@gmail.com>
2017-11-08 20:24:50 -0500

    "Major update to Default Categories"

--- ArticleManagerClient/app/src/main/java/am_utils/DefaultCategories.java
+++ ArticleManagerClient/app/src/main/java/am_utils/DefaultCategories.java
@@ -1,4 +1,4 @@
-package am_utils;
+package com.remaclek.kelcamer.academicarticlemanager;
 
 import java.io.File;
 
@@ -14,29 +14,28 @@ public class DefaultCategories {
      private String mainCategories[] = {
 
              "Biology",
-             "kenya",
-            "Food",
-             "Mailing",
-            "Artificial Intelligence",
-            "Machine Learning",
-             "USPS",
-            "Visible Light Communication",
-            "Infrared",
+             "Food",
              "People",
-             "hello",
-            "Blockchain",
-            "Object Detection",
-            "Transistors",
-            "Nanotechnology",
-            "Quadcopters",
+             "Technology",
+             "Animals",
+             "Social",
+             "Travel",
+             "Life",
+             "Other",
+
+
+             // Under technology
+
 
 
 
     };
 
+
     DefaultCategories(){
         index = -1;
         mainCat = new MainCategory[50];
+
         createCategories();
 
     }
@@ -48,14 +47,124 @@ public class DefaultCategories {
             index++;
         }
 
+        // Biology
+        String[] biologyList = {
+                "Biodiversity",
+                "Molecular",
+                "Human",
+                "Chemical",
+                "Cell"
 
-        categorize();
+        };
+        mainCat[0].addNewSubcategory(biologyList);
 
 
+        // food list
+       String[] foodList = {"Meat", "Turkey",
+                "Chicken",
+                "Seafood",
+                "Eggs",
+                "Milk", "Cheese", "Dairy",
+                "Fruits", "Vegetables"};
+        mainCat[1].addNewSubcategory(foodList);
+
+
+        // People
+        String[] peopleList = {
+
+                "White",
+                "Hispanic",
+                "Black and African Americans",
+                "Asian Americans",
+                "Native Americans and Alaska Natives",
+
+
+        };
+        mainCat[2].addNewSubcategory(peopleList);
+
+        // technology
+        String[] technologyList = {
+                   "Artificial Intelligence",
+                   "Machine Learning",
+                  "Visible Light Communication",
+                   "Infrared",
+                   "Blockchain",
+                   "Object Detection",
+                    "Transistors",
+                    "Nanotechnology",
+                    "Quadcopters",
+                   "Programming",
+
+        };
+        mainCat[3].addNewSubcategory(technologyList);
+
+
+        // Animals
+        String[] animalList = {
+                "Dogs",
+                "Cats",
+                "Bears",
+                "Lions",
+                "Tigers",
+                "Octopus",
+                "Pig",
+                "Cow",
+                "Chicken"
+
+
+        };
+        mainCat[4].addNewSubcategory(animalList);
+
+
+        // Social
+
+        String[] socialList = {
+
+                "Facebook",
+                "Twitter",
+                "Snapchat",
+                "Instagram",
+                "Skype",
+                "MySpace"
+        };
+        mainCat[5].addNewSubcategory(socialList);
+
+
+        // Travel
+
+        String[] travelList={
+            "Airplane",
+                "Bike",
+                "Segway",
+                "Hoverboard",
+                "Skateboard",
+                "Car",
+                "Helicopter"
+
+        };
+        mainCat[6].addNewSubcategory(travelList);
+
+
+        // Life
+
+        String[] lifeList = {
+                "Tools",
+                "Kitchen",
+                "Living",
+                "School",
+                "Work",
+                "Philosophy"
+
+        };
+        mainCat[7].addNewSubcategory(lifeList);
+
+
+
+        // Other has no subcategories and will be defined separately
 
     }
 
-        public void categorize(File file){
+        public void categorize(File uploadedFile){
 
 
 
