e0219f589aab9ac90de1ba5da4a6a125157b0ac5
Noah <32653654+NThering@users.noreply.github.com>
2017-11-29 18:16:55 -0500

    Latest networking stuff for Ian to mess with

--- ArticleManagerClient/app/src/main/java/networking/Public.java
+++ ArticleManagerClient/app/src/main/java/networking/Public.java
@@ -38,7 +38,10 @@ class NetworkingHeartbeatManager extends Thread {
 
             // Send our heartbeat!
             if (connected)
+            {
+                CUtils.msg("Sending heartbeat!");
                 Public.heartBeat();
+            }
         }
     }
 }
@@ -53,17 +56,13 @@ public class Public extends Thread {
 
     // Sign in details
     protected static final String HOSTNAME = "odroid.now-dns.net";
-    protected static final int PORT = 1906;
+    //protected static final String HOSTNAME = "127.0.0.1";
+    protected static final int TEXTPORT = 1906;
+    protected static final int OBJECTPORT = 1907;
+    protected static final int FILEPORT = 1908;
 
     // socket to send/receive data, MUST BE STARTED FIRST
-    protected static Socket clientSocket = getOurSocket();
-
-    // Streams for writing/receiving data.
-    protected static ObjectInputStream inputObject = getOurInputStream();
-    protected static ObjectOutputStream outputObject = getOurOutputStream();
-
-    //PrintWriter for sending data
-    protected static PrintWriter writer = getOurPrintWriter();
+    protected static Socket textSocket = getOurSocket();
 
     //keep connection established (2 to 5 sec heartbeat intervals)
     protected static final NetworkingHeartbeatManager heartbeater = getOurHeartbeater();
@@ -76,53 +75,11 @@ public class Public extends Thread {
     {
         try
         {
-            return new Socket(HOSTNAME, PORT);
+            return new Socket(HOSTNAME, TEXTPORT);
         }
         catch( Exception e )
         {
             CUtils.warning("Failed to create socket!!!");
-            CUtils.warning("Details: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private static PrintWriter getOurPrintWriter()
-    {
-        try
-        {
-            return new PrintWriter(clientSocket.getOutputStream(), true);
-        }
-        catch( Exception e )
-        {
-            CUtils.warning("Failed to create print writer!!!");
-            CUtils.warning("Details: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private static ObjectOutputStream getOurOutputStream()
-    {
-        try
-        {
-            return new ObjectOutputStream(clientSocket.getOutputStream());
-        }
-        catch( Exception e )
-        {
-            CUtils.warning("Failed to create output stream!!!");
-            CUtils.warning("Details: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private static ObjectInputStream getOurInputStream()
-    {
-        try
-        {
-            return new ObjectInputStream(clientSocket.getInputStream());
-        }
-        catch( Exception e )
-        {
-            CUtils.warning("Failed to create input stream!!!");
             CUtils.warning("Details: " + e.getMessage());
             return null;
         }
@@ -165,6 +122,20 @@ public class Public extends Thread {
             return 2;
         }
 
+        PrintWriter writer;
+
+        try
+        {
+            writer = new PrintWriter(textSocket.getOutputStream(), true);
+        }
+        catch( Exception e )
+        {
+            CUtils.warning("Failed to create print writer!!!");
+            CUtils.warning("Details: " + e.getMessage());
+            return 2;
+        }
+
+
         String signal = String.valueOf(opCode);
 
         if (param1 != null)
@@ -178,7 +149,8 @@ public class Public extends Thread {
 
         try
         {
-            writer.write(signal);
+            CUtils.msg("Sending signal: " + signal);
+            writer.println(signal);
             heartbeater.connected = true;
         }
         catch(Exception e)
@@ -192,9 +164,35 @@ public class Public extends Thread {
 
     private static int sendObject(Object object)
     {
+        ObjectOutputStream outputObjectStream;
+        Socket clientSocket;
+
         try
         {
-            outputObject.writeObject(object);
+            clientSocket = new Socket(HOSTNAME, OBJECTPORT);
+        }
+        catch(Exception e)
+        {
+            CUtils.warning("Failed to create object output stream socket!!!");
+            CUtils.warning("Details: " + e.getMessage());
+            return 3;
+        }
+
+        try
+        {
+            outputObjectStream =  new ObjectOutputStream(clientSocket.getOutputStream());
+        }
+        catch( Exception e )
+        {
+            CUtils.warning("Failed to create object output stream!!!");
+            CUtils.warning("Details: " + e.getMessage());
+            return 2;
+        }
+
+        try
+        {
+            outputObjectStream.writeObject(object);
+            clientSocket.close();
             heartbeater.connected = true;
         }
         catch(Exception e)
@@ -211,16 +209,47 @@ public class Public extends Thread {
         int fileSize = (int) file.length();
         byte[] articleByteArray = new byte[fileSize];
 
+        Socket clientSocket;
+
+        try
+        {
+            clientSocket = new Socket(HOSTNAME, FILEPORT);
+        }
+        catch(Exception e)
+        {
+            CUtils.warning("Failed to create file output stream socket!!!");
+            CUtils.warning("Details: " + e.getMessage());
+            return 3;
+        }
+
         try
         {
             FileInputStream fileInStream = new FileInputStream(file);
             BufferedInputStream inStream = new BufferedInputStream(fileInStream);
-            inStream.read(articleByteArray, 0, fileSize);
+
+            //inStream.read(articleByteArray, 0, fileSize);
+
+
+            CUtils.debugMsg("Read file, time to transmit...");
 
             OutputStream outStream = clientSocket.getOutputStream();
-            outStream.write(articleByteArray, 0, fileSize);
+            CUtils.debugMsg("Got output stream...");
+
+            int count = 0;
+            while ((count = fileInStream.read(articleByteArray)) > 0)
+            {
+                outStream.write(articleByteArray, 0, count);
+            }
+
+            inStream.close();
+
+            //outStream.write(articleByteArray, 0, fileSize);
+            CUtils.debugMsg("Wrote file to buffer, time to flush...");
             outStream.flush();
-            outStream.close();
+            CUtils.debugMsg("Flushed, time to close...");
+            clientSocket.close();
+
+            CUtils.debugMsg("Closed!");
 
             heartbeater.connected = true;
         }
@@ -246,15 +275,16 @@ public class Public extends Thread {
     {
         try
         {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(textSocket.getInputStream()));
 
             String returnVal = reader.readLine();
-            reader.close();
+
             return returnVal;
         }
         catch(Exception e)
         {
             CUtils.warning("Failed to receive string!!!");
+            CUtils.warning(e.getMessage());
             return null;
         }
     }
@@ -263,11 +293,41 @@ public class Public extends Thread {
     {
         try
         {
-            return inputObject.readObject();
+            ObjectInputStream inputObject = null;
+            Socket clientSocket;
+
+            try
+            {
+                clientSocket = new Socket(HOSTNAME, OBJECTPORT);
+            }
+            catch(Exception e)
+            {
+                CUtils.warning("Failed to create object input stream socket!!!");
+                CUtils.warning("Details: " + e.getMessage());
+                return null;
+            }
+
+            try
+            {
+                inputObject = new ObjectInputStream( clientSocket.getInputStream() );
+            }
+            catch( Exception e )
+            {
+                CUtils.warning("Failed to create input stream!!!");
+                CUtils.warning("Details: " + e.getMessage());
+                return null;
+            }
+
+            Object receivedObject = inputObject.readObject();
+
+            clientSocket.close();
+
+            return receivedObject;
         }
         catch(Exception e)
         {
             CUtils.warning("Failed to receive any objects!!!");
+            CUtils.warning(e.getMessage());
             return null;
         }
     }
@@ -276,6 +336,18 @@ public class Public extends Thread {
     {
         //byte array for file data
         byte[] articleByteArray = new byte[fileSize];
+        Socket clientSocket;
+
+        try
+        {
+            clientSocket = new Socket(HOSTNAME, FILEPORT);
+        }
+        catch(Exception e)
+        {
+            CUtils.warning("Failed to create file input stream socket!!!");
+            CUtils.warning("Details: " + e.getMessage());
+            return null;
+        }
 
         try
         {
@@ -289,6 +361,7 @@ public class Public extends Thread {
             outStream.write(articleByteArray, 0, bytesReadIn);
             outStream.flush();
             outStream.close();
+            clientSocket.close();
 
             return articleDL;
         }
@@ -332,14 +405,14 @@ public class Public extends Thread {
     public static int logout( )
     {
         sendSignal(2);
-        return 0;
+        return Integer.parseInt(receiveString());
     }
 
     /** Relays user info to the login system on the server, returning 0 on the client if the
      * registration was successful and an int corresponding to the type of error if it was not. */
     public static int register( String username, String password )
     {
-        sendSignal(1, username, password);
+        sendSignal(3, username, password);
 
         return Integer.parseInt(receiveString());
     }
@@ -362,8 +435,11 @@ public class Public extends Thread {
         int errorValue = 0;
 
         errorValue += sendSignal(5, Integer.toString(fileSize));
+        CUtils.debugMsg("Sent signal");
         errorValue += sendObject(articleInfo);
+        CUtils.debugMsg("Sent object");
         errorValue += sendFile(articleFile);
+        CUtils.debugMsg("Sent file");
 
         // Will only return 0 if no errors were encountered.
         return errorValue;
@@ -373,6 +449,7 @@ public class Public extends Thread {
     public static ArticleInfo getArticleInfo( int articleID )
     {
         sendSignal(6, Integer.toString(articleID));
+
         return (ArticleInfo)receiveObject();
     }
 
